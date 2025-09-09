490f24aff9d6028eb66ba7ab43ffe7a9ef4f3fb8
Adbonino <abonino88@gmail.com>
2025-09-09 16:42:13 -0300

    Create scp.py

---
+++ backups/scp.py
@@ -0,0 +1,38 @@
+import paramiko
+import time
+
+# Configuración
+SWITCH_IP = '10.0.0.100'
+SWITCH_USER = 'admin'
+SWITCH_PASS = 'clave_switch'
+
+SERVER_IP = '192.168.1.10'
+SERVER_USER = 'backupuser'
+SERVER_PASS = 'clave_backup'
+BACKUP_PATH = '/home/backupuser/brocade_g720_backup.cfg'
+
+# Conexión SSH
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(SWITCH_IP, username=SWITCH_USER, password=SWITCH_PASS, look_for_keys=False)
+
+shell = client.invoke_shell()
+time.sleep(1)
+shell.recv(1000)
+
+# Ejecutar comando de backup
+shell.send('configupload -protocol scp\n')
+time.sleep(2)
+shell.send(f'{SERVER_IP}\n')
+time.sleep(1)
+shell.send(f'{SERVER_USER}\n')
+time.sleep(1)
+shell.send(f'{BACKUP_PATH}\n')
+time.sleep(1)
+shell.send(f'{SERVER_PASS}\n')
+time.sleep(10)
+
+output = shell.recv(50000).decode('utf-8')
+print(output)
+
+client.close()
